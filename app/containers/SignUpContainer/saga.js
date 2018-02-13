7ff2bb60b201112e7b85f1accb9011d6cab61fd7
Hassan Khokhar <tauqeerhassan66@gmail.com>
2018-02-13 10:07:24 +0500

    changed api url

--- app/containers/SignUpContainer/saga.js
+++ app/containers/SignUpContainer/saga.js
@@ -25,7 +25,7 @@ function* signUp() {
 
 
   try {
-    const res = yield call(axios.post.bind(axios, 'http://localhost:8000/api/users'), data);
+    const res = yield call(axios.post.bind(axios, 'http://ec2-52-201-203-83.compute-1.amazonaws.com/api/users'), data);
     console.log(res);
     // if(res.statusText === "OK"){
     //   SelfieImg = res.data.status;
